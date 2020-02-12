1a12102dd1e475a27e5fc3d291e6b69f7206c2eb
davidmackdev <51126533+davidmackdev@users.noreply.github.com>
2020-02-12 14:35:59 -0500

    Add files via upload

--- simpleCalc.py
+++ simpleCalc.py
@@ -11,20 +11,78 @@ def button_click(number):
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
 
+def button_clear():
+    e.delete(0, END)
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "add"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtract"
+    f_num = int(first_number)
+    e.delete(0, END)
+    return
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiply"
+    f_num = int(first_number)
+    e.delete(0, END)
+    return
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "divide"
+    f_num = int(first_number)
+    e.delete(0, END)
+    return
+
+def button_equal():
+    s_num = int(e.get())
+    e.delete(0, END)
+    if math == "add":
+        e.insert(0, f_num + s_num)
+    if math == "subtract":
+        e.insert(0, f_num - s_num)
+    if math == "multiply":
+        e.insert(0, f_num * s_num)
+    if math == "divide":
+        e.insert(0, f_num / s_num)
+
+
 # Define Buttons
-button_1 = Button(root, text="1", padx=55, pady=20, background="plum", command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=55, pady=20, background="plum", command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=55, pady=20, background="plum", command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=55, pady=20, background="plum", command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=55, pady=20, background="plum", command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=55, pady=20, background="plum", command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=55, pady=20, background="plum", command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=55, pady=20, background="plum", command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=55, pady=20, background="plum", command=lambda: button_click(9))
+button_1 = Button(root, text="1", padx=56, pady=20, background="plum", command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=56, pady=20, background="plum", command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=56, pady=20, background="plum", command=lambda: button_click(3))
+
+button_4 = Button(root, text="4", padx=56, pady=20, background="plum", command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=56, pady=20, background="plum", command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=56, pady=20, background="plum", command=lambda: button_click(6))
+
+button_7 = Button(root, text="7", padx=56, pady=20, background="plum", command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=56, pady=20, background="plum", command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=56, pady=20, background="plum", command=lambda: button_click(9))
+
 button_0 = Button(root, text="0", padx=55, pady=20, background="plum", command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=54, pady=20, background="peach puff", command=lambda: button_click())
-button_equal = Button(root, text="=", padx=117, pady=20, background="peach puff", command=lambda: button_click())
-button_clear = Button(root, text="Clear", padx=106, pady=20, background="peach puff", command=lambda: button_click())
+
+button_add = Button(root, text="+", padx=55, pady=20, background="peach puff", command=button_add)
+button_subtract = Button(root, text="-", padx=56, pady=20, background="peach puff", command=button_subtract)
+button_multiply = Button(root, text="*", padx=56, pady=20, background="peach puff", command=button_multiply)
+button_divide = Button(root, text="/", padx=56, pady=20, background="peach puff", command=button_divide)
+button_equal = Button(root, text="=", padx=118, pady=20, background="peach puff", command=button_equal)
+button_clear = Button(root, text="Clear", padx=109, pady=20, background="peach puff", command=button_clear)
 
 
 # Put the buttons on the screen
@@ -45,6 +103,10 @@ button_clear.grid(row=4, column=1, columnspan=2)
 
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 
 root.mainloop()
